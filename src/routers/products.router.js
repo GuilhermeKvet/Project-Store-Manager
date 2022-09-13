e590590078a78d134eb32ca30124c264ec83bffa
Guilherme Vinicius Kvet da Rocha <guilhermevkvet22@gmail.com>
2022-09-12 21:09:33 -0300

    Realizando requsito 4

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { productsController } = require('../controllers');
+const { validateProduct } = require('../middlewares/validateProduct');
 
 const router = express.Router();
 
 router.get('/', productsController.findAll);
 
-router.post('/', productsController.registerProduct);
+router.post('/', validateProduct, productsController.registerProduct);
 
 router.get('/:id', productsController.findById);
 
